0ba7300583e4e55081409b1ef99cb89cfd21c2c1
Aleksandr <crm.31885@gmail.com>
2025-02-15 18:11:08 +0200

    refactor: move countries to controller

--- backend-the-country-info-app/src/countries/countries.controller.ts
+++ backend-the-country-info-app/src/countries/countries.controller.ts
@@ -1,17 +1,17 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { CountriesService } from './countries.service';
+import { Controller, Get, Param } from "@nestjs/common";
+import { CountriesService } from "./countries.service";
 
-@Controller()
+@Controller("countries")
 export class CountriesController {
   constructor(private readonly appService: CountriesService) {}
 
-  @Get('countries')
+  @Get("")
   getCountries() {
     return this.appService.getCountries();
   }
 
-  @Get('countries/:countryCode')
-  getCountryInfo(@Param('countryCode') countryCode: string) {
+  @Get(":countryCode")
+  getCountryInfo(@Param("countryCode") countryCode: string) {
     return this.appService.getCountryInfo(countryCode);
   }
 }
